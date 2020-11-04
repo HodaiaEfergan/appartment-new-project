46e29e370fe0cd744aa2ce88e4ac669be0116c87
= <hodaia0524289665@gmail.com>
2020-11-04 14:18:24 +0200

    k

--- client/src/app/services/department.service.ts
+++ client/src/app/services/department.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Department } from '../models/department';
-const baseUrl = 'http://localhost:4000';
+//const baseUrl = 'http://localhost:4000';
+const baseUrl = 'https://appartment-new-project.herokuapp.com'
 @Injectable({
   providedIn: 'root'
 })
@@ -9,12 +10,12 @@ export class DepartmentService {
   currentDepartments: Array<Department>
   constructor(private http: HttpClient) { }
   getDepartments(data) {
-    return this.http.post(baseUrl + '/releventHouses',data);
+    return this.http.post(baseUrl + '/releventHouses', data);
   }
   addDepartment(data) {
     return this.http.post(baseUrl + '/add', data)
   }
-  deleteepartment(data){
+  deleteepartment(data) {
     return this.http.post(baseUrl + '/delete', data)
   }
 }
